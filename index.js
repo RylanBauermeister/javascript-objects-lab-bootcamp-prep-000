43af8954ed046b01065a822a5c488eec721896fc
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-05 03:16:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
 function 
